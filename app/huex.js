135e2c780d0999d828c0488d60536bdd6d8b3339
Vitaly Senko <vsianko@exadel.com>
2018-08-30 12:59:15 +0300

    implementation and unit tests

---
+++ app/huex.js
@@ -0,0 +1,37 @@
+let EventEmmiter;
+
+if (require) {
+  EventEmmiter = require('events').EventEmitter;
+} else {
+  //TODO: write solution for FE
+  EventEmmiter = class { }
+}
+
+const proxySettings = {
+  get(target, prop) {
+    if (target[prop] === undefined) {
+      target[prop] = huex();
+    }
+    return target[prop];
+  },
+
+  set(target, prop, value) {
+    if (value instanceof Object) {
+      value = huex(value);
+    }
+
+    target[prop] = value;
+    target.emit('change:' + prop, { value });
+    return true;
+  }
+}
+
+function huex(obj) {
+  // obj = obj || {};
+  // Object.setPrototypeOf(obj, new EventEmmiter());
+  obj = new EventEmmiter();
+  const storage = new Proxy(obj, proxySettings);
+  return storage;
+}
+
+module.exports = huex;
---
+++ app/huex.spec.js
@@ -0,0 +1,34 @@
+const assert = require('assert');
+
+const Huex = require('./huex');
+
+describe('Huex', () => {
+  let sut;
+
+  beforeEach(() => {
+    sut = Huex();
+  });
+
+  it('Should instantiate object', () => {
+    assert.equal(typeof (sut), 'object');
+  });
+
+  it('should save values', () => {
+    sut.a = 5;
+    assert.equal(sut.a, 5);
+  });
+
+  it('should save long path values', () => {
+    sut.a.b.c = 5;
+    assert.equal(sut.a.b.c, 5);
+  });
+
+  it('should fire event on value change', (done) => {
+    sut.on('change:a', (e) => {
+      assert.equal(e.value, 5);
+      done();
+    });
+
+    sut.a = 5;
+  });
+});
